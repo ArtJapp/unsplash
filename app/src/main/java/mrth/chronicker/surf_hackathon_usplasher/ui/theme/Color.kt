50eccbaed9b76aa0d1d8fb510dbbe91c40fde2e1
Artyom Japparov <artyom.japparov@devexperts.com>
2021-12-11 12:02:13 +0400

    uikit initialized

--- app/src/main/java/mrth/chronicker/surf_hackathon_usplasher/ui/theme/Color.kt
+++ app/src/main/java/mrth/chronicker/surf_hackathon_usplasher/ui/theme/Color.kt
@@ -6,3 +6,15 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+
+val Black = Color(0xFF000000)
+val Haiti = Color(0xFF251F26)
+
+val LimeGreen = Color(0xFF30C150)
+val Silver = Color(0xFFC4C4C4)
+val LimeGreenDark = Color(0xFF42DB63) // top line from UIKit
+val SuvaGray = Color(0xFF878787) // top line from UIKit
+
+val WhiteSmoke = Color(0xFFF2F2F2)
+val White = Color(0xFFFFFFFF)
